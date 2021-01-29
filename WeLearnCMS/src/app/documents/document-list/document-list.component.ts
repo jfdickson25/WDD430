b93e5590268e2cc9282c672a752bc996b737373d
Jordan Dickson <jfdickson25@gmail.com>
2021-01-29 10:59:10 -0700

    prove04 completed

--- WeLearnCMS/src/app/documents/document-list/document-list.component.ts
+++ WeLearnCMS/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Document } from '../document.model';
 
 @Component({
@@ -7,12 +7,21 @@ import { Document } from '../document.model';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
   documents: Document[] = [
-    new Document("1", "Document1", "A great document", "#", null )
+    new Document("1", "Document1", "A great document part 1", "#", null ),
+    new Document("2", "Document2", "A great document part 2", "#", null ),
+    new Document("3", "Document3", "A great document part 3", "#", null ),
+    new Document("4", "Document4", "A great document part 4", "#", null ),
+    new Document("5", "Document5", "A great document part 5", "#", null ),
   ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectedDocument = (document: Document) => {
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
